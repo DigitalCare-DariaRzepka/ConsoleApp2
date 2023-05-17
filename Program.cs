f04bae720e78757494089b09dd0fb3c12f09a2bc
Daria Rzepka <daria.rzepka@digitalcaregroup.com>
2023-05-17 14:43:28 +0200

    fff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Helloo, World!");
-Console.WriteLine("hello");
-Console.WriteLine("hellllooooo Daria i Kamil");
+Console.WriteLine("helo");
+Console.WriteLine("hellllooooo Kamil");
