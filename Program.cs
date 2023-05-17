9517bc63153956b82c8ed1561ad397ba29d261e4
Iga Adamczuk <iga.adamczuk@digitalcaregroup.com>
2023-05-17 14:44:07 +0200

    zmiana2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Helloo, World!");
-Console.WriteLine("hello");
+Console.WriteLine("hello dopisuję tutaj COŚ ");
 Console.WriteLine("hellllooooo Daria i Kamil");
