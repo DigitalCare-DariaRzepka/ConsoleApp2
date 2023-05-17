d938bbf09f989a6d6d2713c42987edabe68a6327
Daria Rzepka <daria.rzepka@digitalcaregroup.com>
2023-05-17 14:51:23 +0200

    5

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 Console.WriteLine("Helloo, World!");
 Console.WriteLine("helo");
 Console.WriteLine("hellllooooo Kamil");
+
+for (int i = 0;i < 10; i++)
+{
+    Console.WriteLine((i+1).ToString());
+}
+int a = 0;
+
+while (a< 10)
+{
+    Console.WriteLine((a+15).ToString());
+    a++;
+}
